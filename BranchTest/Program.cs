550bb5011ad68441ee20f52d34b7d42af69c7bcb
Chris Shallcross <ChrisShallcross@hotmail.com>
2018-10-18 16:32:16 +0700

    Added text

--- BranchTest/Program.cs
+++ BranchTest/Program.cs
@@ -7,6 +7,10 @@ namespace BranchTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
